ca181fbd66f0969736ed59e8f56f0aaefb20db27
Malakai <poncemalakai@gmail.com>
2025-09-21 17:31:02 -0700

    Commit 3 -  this is just the main function, I included some changes in the data to show they dynamic-ness of it

--- Lab_11.cpp
+++ Lab_11.cpp
@@ -22,3 +22,32 @@ void printPlayer(const Player& p) {
     }
     cout << endl;
 } 
+
+int main() { 
+    vector<Player> roster;
+                                                    
+    roster.push_back({"Avery", "555-1010", true,  {"Catan"}}); // Add players to the roster with le push_back method
+    roster.push_back({"Bri",   "555-2020", false, {}});              // starts with 0 games
+    roster.push_back({"Cam",   "555-3030", true,  {"Codenames"}});
+
+    // --- Before updates ---
+    cout << "=== Before updates ===\n";
+    for (const auto& p : roster) {
+        printPlayer(p);
+        cout << "--------------------\n";
+    }
+
+                                                 // Change data to show mutability (inner dynamic array grows)
+    roster[1].rsvped = true;                     // Bri RSVPs later
+    roster[1].games.push_back("Uno");            // add games dynamically
+    roster[1].games.push_back("Exploding Kittens");
+
+    // --- After updates ---
+    cout << "=== After updates ===\n";
+    for (const auto& p : roster) {
+        printPlayer(p);
+        cout << "--------------------\n";
+    }
+
+    return 0;
+}
